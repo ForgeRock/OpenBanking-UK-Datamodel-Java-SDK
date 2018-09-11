4a6af38baf64178b2459aa6ec6e10968463cdfc9
Ben Jefferies <benjefferies@echosoft.uk>
2018-09-11 14:47:57 +0100

    Remove whitespace to trigger build

--- release.sh
+++ release.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 # This script will do a release of the artifact according to http://maven.apache.org/maven-release/maven-release-plugin/
-
 git checkout $TRAVIS_BRANCH
 git config --global user.email "travis@travis-ci.org";
 git config --global user.name "Travis CI";
