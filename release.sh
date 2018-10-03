c1e1f26f2ca699f726e7ead43294a67885b5643b
Ben Jefferies <benjefferies@echosoft.uk>
2018-10-03 14:21:13 +0100

    Sign artifacts on release (#16)
    * Sign artifacts on release
    
    To deploy to maven central the artifacts need to be signed. We are using
    the gpg maven plugin for this.
    
    The maven.asc.enc is the gpg key encrypted with the public key of travis.
    This is the recommended way of keeping secrets for travis in github is
    described here https://docs.travis-ci.com/user/encrypting-files/.
    
    We also need to provide the javadoc to publish to maven repo.
    
    * Remove verbose build

--- release.sh
+++ release.sh
@@ -4,5 +4,5 @@
 git checkout $TRAVIS_BRANCH
 git config --global user.email "travis@travis-ci.org";
 git config --global user.name "Travis CI";
-mvn -X -s settings.xml -Dusername=$GITHUB_API_KEY release:prepare -B
-mvn -X -s settings.xml release:perform -Darguments="-Dmaven.javadoc.skip=true"
+mvn -s settings.xml -Dusername=$GITHUB_API_KEY release:prepare -B
+mvn -s settings.xml release:perform -Darguments="-Dgpg.skip=false"
