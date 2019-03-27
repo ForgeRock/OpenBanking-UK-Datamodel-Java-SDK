9991f9e2c18a74ffbddf698fdea519580a5ded18
Quentin CASTEL <quentin.castel@forgerock.com>
2019-03-27 09:46:05 +0000

    Move copyright to 2019

--- src/test/java/uk/org/openbanking/datamodel/service/converter/OBExternalAccountIdentificationConverterTest.java
+++ src/test/java/uk/org/openbanking/datamodel/service/converter/OBExternalAccountIdentificationConverterTest.java
@@ -12,7 +12,7 @@
  *  Header, with the fields enclosed by brackets [] replaced by your own identifying
  *  information: "Portions copyright [year] [name of copyright owner]".
  *
- *  Copyright 2018 ForgeRock AS.
+ *  Copyright 2019 ForgeRock AS.
  */
 package uk.org.openbanking.datamodel.service.converter;
 
