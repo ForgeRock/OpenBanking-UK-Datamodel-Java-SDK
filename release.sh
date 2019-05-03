3cd45cd89f7d7e62276250047348d5e2eef6d3bf
Travis CI <travis@travis-ci.org>
2019-05-03 15:06:23 +0100

    Replace username

--- release.sh
+++ release.sh
@@ -4,5 +4,5 @@
 git checkout $TRAVIS_BRANCH
 git config --global user.email "travis@travis-ci.org";
 git config --global user.name "Travis CI";
-mvn -s settings.xml -Dusername=DH001 release:prepare -B -Ptravis
+mvn -s settings.xml -Dusername=$GITHUB_API_KEY release:prepare -B -Ptravis
 mvn -s settings.xml release:perform -Ptravis
